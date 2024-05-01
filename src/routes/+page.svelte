526b5f6c9b41acb319590747c7753cead00f36ab
Vishal Gupta <49409863+vishalgupta3006@users.noreply.github.com>
2024-05-01 12:39:49 +0000

    Added tailwind and basic API

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
+<script lang="ts">
+
+
+
+const fetchQuestions = async () => {
+    const response = await fetch("/api/get-questions-list");
+    const data = await response.json()
+    return data;
+
+}
+const promise = fetchQuestions();
+
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="w-full bg-black text-bold">hey</div>
+
+
+{#await promise}
+	<p>Loading</p>
+{:then questionList}
+	{#each questionList as questions}
+		
+		<button class="w-1/2 p-4 border border-black rounded-sm">
+			{questions.title}
+		</button>
+	{/each}
+{:catch error}
+	<p>Something went wrong: {error.message}</p>
+{/await}
---
+++ src/routes/api/get-questions-list/+server.ts
@@ -0,0 +1,7 @@
+import { json } from '@sveltejs/kit';
+
+export const GET = async ({  fetch }) => {
+    const resposne = await fetch("https://run.mocky.io/v3/70a2b0a0-4bbf-4a9e-a991-0b55ff1c9ba1");
+    const data = await resposne.json();
+    return json(data);
+};
