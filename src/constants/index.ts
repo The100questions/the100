29c47d1386ff7ad125d55acbb1ca1180eb3ffd56
Vishal Gupta <49409863+vishalgupta3006@users.noreply.github.com>
2024-05-08 19:45:52 +0000

    Added lang cookie and header in every API request

---
+++ src/constants/index.ts
@@ -0,0 +1,7 @@
+export const API_HEADERS = {
+  LANGUAGE: "LANGUAGE",
+};
+
+export const USER_COOKIES = {
+  LANGUAGE: "lang",
+};
---
+++ src/hooks.server.ts
@@ -0,0 +1,26 @@
+import { parseJSON } from "$lib/utils";
+import { API_HEADERS, USER_COOKIES } from "@constants";
+import type { Handle, HandleFetch } from "@sveltejs/kit";
+
+export const handle: Handle = async ({ event, resolve }) => {
+  const langCookie = event.cookies.get(USER_COOKIES.LANGUAGE);
+  const userLang = parseJSON(langCookie)?.name;
+  if (!userLang) {
+    event.cookies.set(
+      USER_COOKIES.LANGUAGE,
+      JSON.stringify({ name: "english" }),
+      {
+        path: "/",
+      }
+    );
+  }
+  const response = await resolve(event);
+  return response;
+};
+
+export const handleFetch: HandleFetch = async ({ event, request, fetch }) => {
+  const langCookie = event.cookies.get(USER_COOKIES.LANGUAGE);
+  const userLang = parseJSON(langCookie)?.name;
+  request.headers.append(API_HEADERS.LANGUAGE, userLang);
+  return fetch(request);
+};
