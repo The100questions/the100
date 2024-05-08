1e197f15582f4fedc08b25a714aa47b151483e96
Vishal Gupta <49409863+vishalgupta3006@users.noreply.github.com>
2024-05-08 20:46:23 +0000

    Added monaco editor

--- src/routes/api/get-questions-list/+server.ts
+++ src/routes/api/get-questions-list/+server.ts
@@ -1,7 +1,9 @@
-import { json } from '@sveltejs/kit';
+import { json } from "@sveltejs/kit";
 
-export const GET = async ({  fetch }) => {
-    const resposne = await fetch("https://run.mocky.io/v3/70a2b0a0-4bbf-4a9e-a991-0b55ff1c9ba1");
-    const data = await resposne.json();
-    return json(data);
+export const GET = async ({ fetch, request }) => {
+  const resposne = await fetch(
+    "https://run.mocky.io/v3/70a2b0a0-4bbf-4a9e-a991-0b55ff1c9ba1"
+  );
+  const data = await resposne.json();
+  return json(data);
 };
---
+++ src/routes/problem/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import MonacoEditor from "$lib/components/MonacoEditor.svelte";
+  import MainWrapper from "$lib/components/ui/MainWrapper.svelte";
+  import * as Breadcrumb from "$lib/components/ui/breadcrumb";
+  import { Header } from "$lib/components/ui/header";
+  let name = "vishal";
+
+
+  let fullName = name + "gupta";
+
+</script>
+
+<Header />
+<MainWrapper>
+  <div>Problem page</div>
+  <div class="flex w-full p-4">
+    <div class="h-full w-full border border-black flex">
+      <div class="flex-1 border border-gray-200">
+        <Breadcrumb.Root>
+          <Breadcrumb.List>
+            <Breadcrumb.Item>
+              <Breadcrumb.Link href="/">Home</Breadcrumb.Link>
+            </Breadcrumb.Item>
+            <Breadcrumb.Separator />
+            <Breadcrumb.Item>
+              <Breadcrumb.Link href="/components">Components</Breadcrumb.Link>
+            </Breadcrumb.Item>
+            <Breadcrumb.Separator />
+            <Breadcrumb.Item>
+              <Breadcrumb.Page>Breadcrumb</Breadcrumb.Page>
+            </Breadcrumb.Item>
+          </Breadcrumb.List>
+        </Breadcrumb.Root>
+      </div>
+      <MonacoEditor />
+    </div>
+  </div>
+</MainWrapper>
