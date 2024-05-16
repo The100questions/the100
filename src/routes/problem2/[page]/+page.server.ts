c2bd0661717d9a46d0bbfdece0e827bad0fb3746
Vishal Gupta <49409863+vishalgupta3006@users.noreply.github.com>
2024-05-16 17:04:44 +0000

    test commit

--- src/routes/problem2/[page]/+page.server.ts
+++ src/routes/problem2/[page]/+page.server.ts
@@ -1,6 +1,7 @@
 export const load = async (event) => {
     const response = await event.fetch("/api/get-questions-list");
     const data = await response.json();
+    
     return {
       question: data[Math.floor(Math.random() * 99)],
     };
