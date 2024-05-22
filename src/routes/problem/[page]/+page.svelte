536c8a1162fb21e5384eb7946002393e316e3a1e
vishalgupta3006 <vishalgupta3006@gmail.com>
2024-05-22 19:10:53 +0000

    removed duplicate questions page

--- src/routes/problem/[page]/+page.svelte
+++ src/routes/problem/[page]/+page.svelte
@@ -1,24 +1,45 @@
-<script>
-  import { CodeEditor } from "$lib/components/ui/code-editor";
+<script lang="ts">
+  import { onMount } from "svelte";
+  import * as Resizable from "$lib/components/ui/resizable";
 
   export let data;
-  const {question} = data ??{};
+  const { question } = data ?? {};
+  let CodeEditorComponent: any;
+
+  onMount(async () => {
+    CodeEditorComponent = (await import(`$lib/components/ui/code-editor`))
+      .CodeEditor;
+  });
 </script>
 
-<div class="size-full p-3">
-  <div class="size-full flex">
-    <div class="flex-1 border rounded overflow-y-auto">
-      <div>it is question {question?.title}</div>
-      <div>it is question {question?.subTitle}</div>
-      <div>it is question {question?.description}</div>
-      <div class="h-[500px] bg-red-100">sdx
-        </div>
-        <div class="h-[500px] bg-red-200">sdx
-        </div>
-
+<Resizable.PaneGroup direction="horizontal" class="max-w-full">
+  <Resizable.Pane defaultSize={50}>
+    <div class="size-full overflow-y-auto px-4">
+      <div class="border rounded">
+        <div>it is question {question?.title}</div>
+        <div>it is question {question?.subTitle}</div>
+        <div>it is question {question?.description}</div>
+        <div class="h-[500px] bg-red-100">sdx</div>
+        <div class="h-[500px] bg-red-200">sdx</div>
+      </div>
     </div>
-    <div class="flex-1 border rounded">
-      <CodeEditor />
-    </div>
-  </div>
-</div>
+  </Resizable.Pane>
+  <Resizable.Handle withHandle />
+  <Resizable.Pane defaultSize={50}>
+    <Resizable.PaneGroup direction="vertical">
+      <Resizable.Pane defaultSize={50}>
+        <div class="size-full flex items-center justify-center">
+          {#if CodeEditorComponent}
+            <svelte:component this={CodeEditorComponent} />
+          {/if}
+        </div>
+      </Resizable.Pane>
+      <Resizable.Handle withHandle />
+      <Resizable.Pane defaultSize={50}>
+        <div class="flex h-full items-center justify-center p-6">
+          <span class="font-semibold">Three</span>
+        </div>
+      </Resizable.Pane>
+    </Resizable.PaneGroup>
+  </Resizable.Pane>
+</Resizable.PaneGroup>
--- src/routes/problem2/[page]/+page.server.ts
+++
@@ -1,9 +0,0 @@
-export const load = async (event) => {
-    const response = await event.fetch("/api/get-questions-list");
-    const data = await response.json();
-    
-    return {
-      question: data[Math.floor(Math.random() * 99)],
-    };
-  };
-  
