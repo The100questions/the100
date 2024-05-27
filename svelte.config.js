25f3f549026bdc09afb0820d36d6eb1b8e7dfd4f
vishalgupta3006 <vishalgupta3006gmail.com>
2024-05-27 22:04:24 +0530

    changed adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,8 @@
-import adapter from "amplify-adapter";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from "@sveltejs/adapter-vercel";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
   preprocess: vitePreprocess(),
@@ -12,7 +11,9 @@ const config = {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter(),
+    adapter: adapter({
+      runtime: "nodejs20.x",
+    }),
     alias: {
       "@constants": "./src/constants",
       "@services": "./src/services",
@@ -22,7 +23,7 @@ const config = {
       directives: {
         "script-src": ["'self'", "'unsafe-eval'"],
       },
-    }
+    },
   },
 };
 
